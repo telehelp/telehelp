4b5b8fcdf4d3854aa15d3ef0fe8b4240eaa32ae1
dekvall <dkvldev@gmail.com>
2020-04-03 23:49:06 +0200

    add login required

--- server/api.py
+++ server/api.py
@@ -1,8 +1,15 @@
 import time
 from flask import Flask
+from middlewares import login_required #Should maybe be properly relative
 
 app = Flask(__name__)
 
 @app.route('/time')
 def current_time():
     return {'time': time.time()}
+
+@app.route('/test', methods=["GET"])
+@login_required
+def test():
+    return {'entry': 'test'}
+
