ee7163b2db4e7f91572871658f6bdd9cea58e90b
Ubuntu <ubuntu@ip-172-31-37-160.eu-west-3.compute.internal>
2020-04-04 08:33:32 +0000

    Added code for serving static files form the react app

--- server/api.py
+++ server/api.py
@@ -1,7 +1,11 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/time')
 def current_time():
