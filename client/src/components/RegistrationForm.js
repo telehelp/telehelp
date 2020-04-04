d29d3f306dde29b2584be2488a1aba9e760a66a9
dekvall <dkvldev@gmail.com>
2020-04-04 17:27:06 +0200

    add faq and make page a bit nicer

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Form, FormFeedback, FormGroup, Label, Input } from 'reactstrap';
 
 function RegistrationForm() {
   const { register, handleSubmit, errors } = useForm(); // initialise the hook
   const onSubmit = data => {
     console.log(data);
-    
+    if (data )
     fetch('/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -14,39 +15,49 @@ function RegistrationForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="display-flex">
-        <div className="block">
-        <label> Name
-        <input name="firstname" ref={register({ required: true })} />
-        </label>
-        {errors.firstname && '(Please enter your name.)'}
-        </div>
-
-        <div className="block">
-        <label> Zip code
-        <input name="zipcode" ref={register({ pattern: /\d+/, required: true })} />
-        </label>
-        {errors.zipcode && '(Please enter your zipcode.)'}
-        </div>
-
-        <div className="block">
-        <label> Phone number
-        <input name="phonenumber" ref={register({ pattern: /\d+/, required: true })} />
-        </label>
-        {errors.zipcode && '(Please enter a valid phone number.)'}
-
-        </div>
-        <div className="block">
-        <label>
-        <input type="checkbox" name="terms" ref={register({required: true})} />
-        I accept the terms and conditions 
-        </label>
-        {errors.zipcode && '(You must accept the terms and conditions)'}
-        </div>
-        <div className="block">
-        <input type="submit" value="Sign up"/>
-        </div>
-    </form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+    <FormGroup>
+        <Label for="helperName">Tilltalsnamn</Label>
+        <Input 
+            name="helperName"
+            id="helperName"
+            placeholder="Bengan"
+            invalid={'helperName' in errors}
+            innerRef={register({ required: true })}/>
+        <FormFeedback invalid></FormFeedback>
+    </FormGroup>
+     <FormGroup>
+     <Label for="zipCode">Postkod</Label>
+        <Input 
+            name="zipCode"
+            id="zipCode"
+            placeholder="12345"
+            invalid={'zipCode' in errors}
+            innerRef={register({pattern: /\d+/, required: true })}/>
+        <FormFeedback invalid>Den postkod som du skrev in är ogilitig</FormFeedback>
+      </FormGroup>
+      <FormGroup>
+     <Label for="phoneNumber">Telefonnummer</Label>
+        <Input 
+            name="phoneNumber"
+            id="phoneNumber"
+            placeholder="0701234567"
+            invalid={'phoneNumber' in errors}
+            innerRef={register({pattern: /\d+/, required: true })}/>
+        <FormFeedback invalid>Det telefonnummer som du skrev in är ogilitig</FormFeedback>
+      </FormGroup>
+      <FormGroup>
+      <Input 
+            type="checkbox"
+            name="terms"
+            id="terms"
+            invalid={'terms' in errors}
+            innerRef={register({required: true })}/>
+        <Label for="terms">Jag accepterar användarvillkoren</Label>
+        <FormFeedback invalid>Du måste acceptera användarvillkoren</FormFeedback>
+      </FormGroup>
+      <Input type="submit" value="Registrera!"/>
+    </Form>
   );
 }
 
