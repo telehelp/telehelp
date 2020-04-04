8d2da8d14ec247f404fb699db31303698cacfdd8
dekvall <dkvldev@gmail.com>
2020-04-04 22:35:20 +0200

    add eggshell color and some more info

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import RegistrationForm from './components/RegistrationForm';
 import FAQ from './components/FAQ';
 import TeleHelpBar from './components/TeleHelpBar';
-import TermsAndConditions from './components/TermsAndConditions';
+import TeleHelpIntroduction from './components/TeleHelpIntroduction'
 
 class App extends React.Component {
   constructor(props) {
@@ -24,41 +24,49 @@ class App extends React.Component {
   }
 
   render() {
-    const {time} = this.state;
+    const { time } = this.state;
 
     return (
     <div className="App">
       <div className="container">
-      <div className="row">
+        <div className="row">
           <div className="col-12">
             <TeleHelpBar/>
           </div>
         </div>
         <div className="row">
-          <div className="col-xs-8 col-8">
+          <div className="col-12">
+            <TeleHelpIntroduction/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
             <div className="introduction">
-              <h1> Registrera dig som hjälpare redan idag!</h1>
+              <h2> Registrera dig som hjälpare redan idag!</h2>
               <p>The current time is {time}.</p>
+              <div className="row">
+                <div className="col-6">
+                  <img className="photo" src="/young-person.png" alt="Young person bringing bag to a door"></img>
+                </div>
+                <div className="col-6">
+                  <img className="photo" src="/old-people.png" alt="Two happy old people on a bench"></img>
+                </div>
+              </div>
               <p>
               Genom att skriva upp dig som volontär kan personer som ingår i riskgrupper eller som redan är smittade av corona i ditt närområde ringa till dig för att få hjälp med sysslor som plötsligt blivit svåra på grund av coronakrisen, exempelvis att handla mat eller hämta mediciner.
               </p>
             </div>
           </div>
-          <div className="col-xs-4">
+          <div className="col-4">
             <div className="signup" id="register">
               <RegistrationForm/>
             </div>
           </div>
+        </div>
         <div className="row">
           <div className="col-12">
             <FAQ/>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <TermsAndConditions/>
-          </div>
-        </div>
         </div>
       </div>
     </div>
