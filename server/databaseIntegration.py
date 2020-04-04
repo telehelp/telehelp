a5dec4ee56439ffb4343943e032c7f69e0e3d98c
Therese Persson <therese.persson.94@gmail.com>
2020-04-04 19:29:39 +0200

    Write customer data to database when calling

--- server/databaseIntegration.py
+++ server/databaseIntegration.py
@@ -2,34 +2,62 @@ import sqlite3
 import pandas as pd
 
 def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
+	""" create a database connection to the SQLite database
+		specified by the db_file
+	:param db_file: database file
+	:return: Connection object or None
+	"""
+	conn = None
+	try:
+		conn = sqlite3.connect(db_file)
+	except Error as e:
+		print(e)
  
-    return conn
+	return conn
 
 
 def fetchData(query, db):
-    conn = create_connection(db)
-    #cur = conn.cursor() 
-    data = pd.read_sql_query(query, conn)
-    conn.close()
-    return data
-    
+	conn = create_connection(db)
+	#cur = conn.cursor() 
+	data = pd.read_sql_query(query, conn)
+	conn.close()
+	return data
+
+def writeToDatabase(db, query, params):
+	try:
+		conn = create_connection(db)
+		cursor = conn.cursor()
+		cursor.execute(query, params)
+		conn.commit()
+		conn.close()
+		return 'Success'
+	except:
+		return 'Failure'    
+
+def savePostcodeToDatabase(db, phone, zipcode, userType):
+	print("Writing phone and postcode to database")
+	print('zipcode:', zipcode, '\nphone: ', phone)
+	if userType == 'customer':
+		query = ''' INSERT INTO user_customers (phone, zipcode) 
+										values(?, ?) '''
+	elif userType == 'helper':
+		query = ''' INSERT INTO user_helpers (phone, zipcode) 
+										values(?, ?) '''		
+	params = (phone, zipcode)
+	flag = writeToDatabase(db, query, params)
+	print(flag)
+	return flag
+	
+	
 def getHelpers(db='telehelp.db'):
-    query = "SELECT * FROM user_helpers"   
-    return fetchData(query, db)
+	query = "SELECT * FROM user_helpers"   
+	return fetchData(query, db)
 
 def getCustomers(db='telehelp.db'):
-    query = "SELECT * FROM user_customers"   
-    return fetchData(query, db)
+	query = "SELECT * FROM user_customers"   
+	return fetchData(query, db)
+
 
 if __name__ == '__main__':
-    getHelpers()
+	print(savePostcodeToDatabase('telehelp.db', '125', '17070', 'customer'))
+	print(getCustomers(db='telehelp.db'))
