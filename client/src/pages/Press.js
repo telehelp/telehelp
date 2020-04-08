6808f573a2b1285de8acd4348dd44ffd238c315b
dekvall <dkvldev@gmail.com>
2020-04-08 15:49:13 +0200

    add press from bonnier

--- client/src/pages/Press.js
+++ client/src/pages/Press.js
@@ -15,6 +15,12 @@ const Press = () => {
             "link": "https://www.kth.se/aktuellt/nyheter/hoppfulla-resultat-av-hack-the-crisis-1.973649",
             "description": "Tyvärr är bara en av oss med i artikeln, trots att alla som gjorde ett fantastiskt arbete"
         },
+        {
+            "header": "Digital hälsa",
+            "name": "Telehelp en av vinnarna i svenska ”Hack the crisis”",
+            "link": "https://app.red.bbmbonnier.se/e/es?s=355424421&e=913550&elqTrackId=aab2e8d998ac428ab064b29999b8fa67&elq=3301dc131494412b98482696742f133e&elqaid=32630&elqat=1",
+            "description": "Återigen är bara en av oss med i artikeln, men vi hoppas på en samlad intervju i framtiden"
+        },
     ]
 
     return (
