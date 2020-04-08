7b83b5270576cb65661f757491865716951ab0e4
Johan Ehrenfors <johanehr@kth.se>
2020-04-08 22:54:53 +0200

    Include J+T in About page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,9 +4,21 @@ const About = () => {
     const people = [
         {
             "name": "David",
-            "responisbility": "Webmaster",
+            "responsibility": "Webmaster",
             "about": "Studerar elektroteknik och robotik vid KTH",
             "email": "david@telehelp.se"
+        },
+        {
+            "name": "Johan Ehrenfors",
+            "responsibility": "Backend developer",
+            "about": "Konsult åt Ericsson hos Cybercom",
+            "email": "johan@telehelp.se"
+        },
+        {
+            "name": "Therese Persson",
+            "responsibility": "Backend developer",
+            "about": "Data engineer hos Sellpy",
+            "email": "therese@telehelp.se"
         }
     ]
     return (
@@ -14,7 +26,7 @@ const About = () => {
             {people.map((e, i) => {
                 return <div key={i}>
                         <h3>{e.name}</h3>
-                        <h4>{e.responisbility}</h4>
+                        <h4>{e.responsibility}</h4>
                         <p>{e.about}</p>
                         Kontakt: <a href={"mailto:" + e.email}>{e.email}</a>
                     </div>
