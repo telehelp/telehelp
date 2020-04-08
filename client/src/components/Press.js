3d8af8eed05ecb3681acfb64a7216d6b19381a5a
dekvall <dkvldev@gmail.com>
2020-04-08 10:51:00 +0200

    add kth news

--- client/src/components/Press.js
+++ client/src/components/Press.js
@@ -7,7 +7,14 @@ const Press = () => {
             "header": "Vinnare av Hack The Crisis 2020",
             "name": "Hack the Crisis pressmeddelande",
             "link": "https://www.mynewsdesk.com/se/hack-for-sweden/pressreleases/winners-of-hack-the-crisis-2989133",
-            "description": "Det var några långa dagar och nätter, men det gick bra tillslut"}
+            "description": "Det var några långa dagar och nätter, men det gick bra tillslut"
+        },
+        {
+            "header": "KTH aktuellt",
+            "name": "Hoppfulla resultat av Hack the Crisis",
+            "link": "https://www.kth.se/aktuellt/nyheter/hoppfulla-resultat-av-hack-the-crisis-1.973649",
+            "description": "Tyvärr är bara en av oss med i artikeln, trots att alla som gjorde ett fantastiskt arbete"
+        },
     ]
 
     return (
