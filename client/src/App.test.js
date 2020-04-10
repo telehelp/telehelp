279681f071c32dfcc38db6ced24be368a2d606c5
dekvall <dkvldev@gmail.com>
2020-04-10 14:57:50 +0200

    add react test suite

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,46 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { FormStatus } from "./actions";
+import { BrowserRouter } from "react-router-dom";
+import MutationObserver from "mutation-observer";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/The time is/i);
+global.MutationObserver = MutationObserver;
+
+test("renders header", () => {
+  const initialState = {
+    registration: { progress: FormStatus.REGISTER_DETAILS },
+  };
+  const mockStore = configureStore();
+
+  let store = mockStore(initialState);
+  const { getByText } = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+  const linkElement = getByText(/Hjälp dina grannar att klara vardagen!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("test registration regex", () => {
+  const initialState = {
+    registration: { progress: FormStatus.REGISTER_DETAILS },
+  };
+  const mockStore = configureStore();
+
+  let store = mockStore(initialState);
+  const { getByText } = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+  const linkElement = getByText(/Hjälp dina grannar att klara vardagen!/i);
   expect(linkElement).toBeInTheDocument();
 });
