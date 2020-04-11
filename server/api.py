16e082b30e62382307efadbef6597b41e54ca389
dawierha <daniel.e_95@hotmail.com>
2020-04-11 11:10:12 +0200

    updated media url

--- server/api.py
+++ server/api.py
@@ -82,8 +82,8 @@ checkEnv(DATABASE_KEY, "DATABASE_KEY")
 checkEnv(SECRET_KEY, "SECRET_KEY")
 
 ZIPDATA = "SE.txt"
-MEDIA_FOLDER = "media/sv"
-MEDIA_URL = "https://media.telehelp.se/media"
+MEDIA_FOLDER = "media"
+MEDIA_URL = "https://media.telehelp.se/media/sv"
 ELK_BASE = "https://api.46elks.com"
 TRUSTED_PROXY = ["127.0.0.1"]
 ELK_USER_AGENT = "46elks/0.2"
