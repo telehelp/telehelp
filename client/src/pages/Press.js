3a0271bd7263bb773d1185f90b79c4b64844cb7e
dekvall <dkvldev@gmail.com>
2020-04-15 17:46:59 +0200

    add mitti stockholm article

--- client/src/pages/Press.js
+++ client/src/pages/Press.js
@@ -16,7 +16,7 @@ const Press = () => {
       link:
         "https://www.kth.se/aktuellt/nyheter/hoppfulla-resultat-av-hack-the-crisis-1.973649",
       description:
-        "Tyvärr är bara en av oss med i artikeln, trots att alla som gjorde ett fantastiskt arbete",
+        "Tyvärr är bara en av oss med i artikeln, trots att alla gjorde ett fantastiskt arbete",
     },
     {
       header: "Digital hälsa",
@@ -25,6 +25,13 @@ const Press = () => {
         "https://app.red.bbmbonnier.se/e/es?s=355424421&e=913550&elqTrackId=aab2e8d998ac428ab064b29999b8fa67&elq=3301dc131494412b98482696742f133e&elqaid=32630&elqat=1",
       description:
         "Återigen är bara en av oss med i artikeln, men vi hoppas på en samlad intervju i framtiden",
+    },
+    {
+      header: "Mitti Stockholm",
+      name: "Nystartad hjälptelefon ska stötta äldre i krisvardagen",
+      link: "https://mitti.se/nyheter/nystartad-hjalptelefon-krisvardagen/",
+      description:
+        "En bild på oss alla (nästan) tillsammans och en härligt skriven artikel",
     },
   ];
 
