1845d9be359f4b3c468c8aeb5ce47e42be2d51cc
Sara Danielsson <sara.v.danielsson@gmail.com>
2020-04-17 13:55:00 +0200

    new text, styling of crash message

--- client/src/components/CrashNotice.js
+++ client/src/components/CrashNotice.js
@@ -2,17 +2,10 @@ import React from "react";
 
 const CrashNotice = () => {
   return (
-    <div className="crash-notice">
-      <h2>Registrera dig även om du redan gjort det!</h2>
+    <div class="alert alert-info" role="alert">
+      <h3 class="alert-heading">Till er som registrerat er som volontärer</h3>
       <p>
-        Natten till den 16 april förlorade vi de flesta användare i en
-        serverkrasch, och vår databas gick tyvärr inte att återställa. Vi har
-        jobbat hårt med att utöka funktionaliten de senaste dagarna och
-        backuprutiner var en av många saker på den listan. Dessa rutiner är nu
-        på plats, om än något sent, för att något liknande aldrig ska hända
-        igen. Vi vill också passa på att tacka er nära 100 användare som
-        hittills har registrerat er och bara är ett telefonsamtal ifrån de som
-        behöver er hjälp.
+        Natten till den 16 april kraschade vår server, och vår databas med era uppgifter gick tyvärr inte att återställa. Vi har jobbat hårt med att utöka funktionaliten de senaste dagarna och backuprutiner var en av många saker på den listan. Dessa rutiner är nu på plats, om än något sent, för att något liknande aldrig ska hända igen. <strong>Därför får vi be er som redan registrerat er, att registrera er igen.</strong> Tack för att ni vill vara med och göra en insats för de som behöver hjälp!
       </p>
     </div>
   );
